83e3e3b3e19954787dbaab4894777661e4900055
James Shen <hummus@mac.lan>
2025-12-22 14:14:22 -0500

    added bloglist page

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,7 +1,22 @@
+import { getAllPosts } from "../../../lib/posts"
+import BlogCard from '@/components/BlogCard';
+
 export default function Page() {
+  const posts = getAllPosts();
+  const recent = posts.slice(0,5);
   return (
-    <div>
-      Notes
+    <div className="flex flex-col items-center">
+      <div className="mt-5 font-bold text-2xl sm:text-4xl border-b border-[#E5E5E5]">
+        All Notes
+      </div>
+      <div className="my-5 grid grid-cols-1 sm:grid-cols-2 gap-10">
+        {recent.map((post) => (
+          <BlogCard key={post.slug}
+          post={post} />
+        ))}
+  
+
+      </div>
     </div>
   )
 }
