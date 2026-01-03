1dcf61b5052283acf5bbd66dfde178e7d719435e
James Shen <hummus@mac.lan>
2026-01-03 10:23:34 -0500

    edited single blog page

--- src/app/notes/[slug]/page.tsx
+++ src/app/notes/[slug]/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <div className="grid grid-cols-1 md:grid-cols-[1fr_320px]">
         <main className="">
           <article className="w-full"> 
-            <div className="w-full aspect-video relative mb-8">
+            <div className="w-full aspect-video relative mb-4">
               <Image 
                 src="/chromakopia-thumbnail.jpeg"
                 alt="thumbnail"
@@ -16,11 +16,11 @@ export default function Page() {
               />
 
             </div>
-            <div className="max-w-[65ch] mx-auto px-5 md:px-8">
-              <p className="text-xl sm:text-3xl font-extrabold mb-2 sm:mb-5 hover:cursor-pointer">
+            <div className="px-2 md:px-6 mx-auto">
+              <p className="text-xl sm:text-3xl font-extrabold mb-2 sm:mb-4 hover:cursor-pointer">
                 Why Chromakopia was released at the peak of Tyler's career
               </p>
-              <div className="nav-links text-sm sm:text-lg text-[#919191] flex gap-2 mb-2 sm:mb-5">
+              <div className="nav-links text-sm sm:text-lg text-[#919191] flex gap-2 mb-2 sm:mb-4">
                 <a>
                   James Shen
                 </a>
@@ -28,7 +28,7 @@ export default function Page() {
                   Dec 22, 2025
                 </a>
               </div>
-              <div className="text-md sm:text-xl">
+              <div className="text-base sm:text-lg">
                 <p>
                   Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc.
                 </p>
