0373931644de96a4c885d697d914a785255c7698
James Shen <hummus@mac.lan>
2026-01-02 20:57:42 -0500

    worked on footer + responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,20 @@ export default function Footer() {
   return (
     // Outer container: full width, bottom border
     <footer className="w-full border-t-2 border-[#E5E5E5]">
-      <div className="justify-between items-center flex py-4 px-5 mx-auto">
+      <div className="justify-between items-center flex pt-4 pb-4 sm:pb-0 px-5 mx-auto">
+        <nav className="text-sm sm:text-base font-bold text-[#9191919] flex flex-col nav-links">
+          <a>Notes</a>
+          <a>About</a>
+          <a>Contact</a>
+
+        </nav>
+        <div className="flex-col sm:flex-row flex gap-2 sm:gap-5">
+          <p className="text-sm sm:text-base font-bold">Subscribe to the newsletter</p>
+          <input className="border-2 border-[#919191] rounded-sm" placeholder="Enter your email" type="email" />
+        </div>
+
+      </div>
+      <div className="justify-between items-center flex pb-4 px-5 mx-auto">
         <p className="text-sm sm:text-base md:text-lg">
           powered by <span className="font-extrabold">hummus</span>
         </p>
