2ca1f2abb9c15f3b372fb6c692ff6490376898c2
James Shen <hummus@mac.mynetworksettings.com>
2026-02-08 17:35:47 -0500

    blog cards linked with slugs

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,39 +1,46 @@
 import Image from "next/image";
 import type { Post } from "../lib/posts";
+import Link from "next/link";
+
+
 
 
 export default function BlogCard({ post }: {post: Post}) {
   return (
-    <div className="flex flex-col gap-2 max-w-[350px]">
-      <div className="w-full aspect-video relative">
-        <Image 
-          src={post.thumbnail}
-          alt={post.title}
-          layout="fill"
-          objectFit="cover"
-          className="object-top"
-        />
-      </div>
-      <p className="text-xl font-bold">
-          {post.title}
-      </p>
-      <div className="nav-links text-sm flex gap-2 text-[#919191]">
-        <a>
-          {post.author}
+    <Link href={`/notes/${post.slug}`}>
+      <div className="flex flex-col gap-2 max-w-[350px]">
+        <div className="w-full aspect-video relative">
+          <Image 
+            src={post.thumbnail}
+            alt={post.title}
+            layout="fill"
+            objectFit="cover"
+            className="object-top"
+          />
+        </div>
+        <p className="text-xl font-bold">
+            {post.title}
+        </p>
+        <div className="nav-links text-sm flex gap-2 text-[#919191]">
+          <a>
+            {post.author}
 
-        </a>
-        <a>
-          {post.date}
-        </a>
-      </div>
-      <div className="line-clamp-3 text-md">
-        {post.body}
+          </a>
+          <a>
+            {post.date}
+          </a>
+        </div>
+        <div className="line-clamp-3 text-md">
+          {post.body}
 
 
 
-      </div>
+        </div>
+        
       
+      </div>
+
+    </Link>
     
-    </div>
   )
 }
