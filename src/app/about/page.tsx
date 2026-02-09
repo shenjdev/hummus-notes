c02f2580e00319fc417ff52e1d638837c92a6bba
James Shen <hummus@mac.mynetworksettings.com>
2026-02-09 13:33:16 -0500

    about and contact pages under construction

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image"
+
 export default function Page() {
   return (
     <div>
+      <div>
+        <Image 
+          src="/page-under-construction.jpg"
+          height={1000}
+          width={1000}
+          alt="yolo"
+          />
+      </div>
       About
     </div>
   )
