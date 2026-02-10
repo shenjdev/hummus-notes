88c800a94d836f9f28231fcd7cd633b7993a09ae
James Shen <hummus@mac.mynetworksettings.com>
2026-02-10 13:57:11 -0500

    feat: added password verification. next: cookie

--- src/app/api/admin/login/route.ts
+++ src/app/api/admin/login/route.ts
@@ -1,7 +1,30 @@
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(req: Request) {
   console.log("ADMIN LOGIN ROUTE HIT");
+
+  // Read request
+  const body = await req.json().catch(() => ({}));
+  const { password } = body;
+
+  // Validate input
+  if (!password) {
+    return NextResponse.json(
+      { error: "Password required" },
+      { status: 400 }
+
+    )
+  }
+
+  // Check against .env password
+  if (password !== process.env.ADMIN_PASSWORD) {
+    return NextResponse.json(
+      { error: "Invalid password "},
+      { status: 401 }
+    )
+  }
+
+  // Passed all the cases! Success!!
   return NextResponse.json({ ok: true });
 }
 
